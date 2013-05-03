d58537d53697be2a042a65e056cc3ce5584b1342
Daniel Hatcher <Daniel.Jon.hatcher@gmail.com>
2013-05-03 14:54:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintOnRails::Application.config.secret_token = 'f81e8f734a01bfe923542e2ea578ae0d49346a564f720cc03c8101a22babc6773f0bb64c2bc21ef178f8acd74c9f40b9f612a4b863e8e168bbfa8d903536ca5d'
