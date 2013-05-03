028d91f7c7d033b066ddd63e6647383cdabc916d
Daniel Hatcher <Daniel.Jon.hatcher@gmail.com>
2013-05-03 15:12:35 -0400

    customer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503190718) do
+ActiveRecord::Schema.define(:version => 20130503190907) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -53,5 +53,13 @@ ActiveRecord::Schema.define(:version => 20130503190718) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "customers", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
