ecafe0c1fce3b599ea9d60ea377893862b136a93
Daniel Hatcher <Daniel.Jon.hatcher@gmail.com>
2013-06-07 09:40:03 -0400

    another retry for the db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create([{email: 'admin@example.com', password: 'Dano1919', password_confirmation: 'Dano1919', role: roles[0]}])
+admin_users = AdminUser.create([{email: 'admin@example.com', password: 'Dano1919', password_confirmation: 'Dano1919', role: roles[0]}])
