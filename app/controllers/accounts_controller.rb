65e4b9ad82a642389d9e218be0076543ceb5b45f
Matthew Norton <mnort9@gmail.com>
2013-10-18 17:45:49 -0400

    User is created in accounts controller instead of nested form

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,5 +5,19 @@ class AccountsController < InheritedResources::Base
 
 	def create
     @account = Account.new(params[:account])
+    @account.save
+
+    user = @account.admin_users.new
+    user.email = params[:email]
+    user.password = params[:password]
+    user.password_confirmation = params[:password_confirmation]
+    user.role = params[:role]
+    user.first = params[:first]
+    user.last = params[:last]
+    user.save
+
+    sign_in(user)
+
+    redirect_to admin_account_path(@account)
 	end
 end
