8aec03370b1eaaef09d5098a7ef83abef5e2e67a
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-10-24 19:45:06 -0400

    cleaned up application controller . rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,9 @@ class ApplicationController < ActionController::Base
     admin_user_signed_in?
   end
 
-
-  def current_ability
+  def current_ability #cancan required!
     @current_ability ||= Ability.new(current_user)
   end
-  
   
   rescue_from CanCan::AccessDenied do |exception|
     begin
@@ -21,11 +19,10 @@ class ApplicationController < ActionController::Base
       redirect_to admin_dashboard_path, :alert => exception.message
     end
   end
-#
-  def user_for_paper_trail
+
+  def user_for_paper_trail #auditing
     admin_user_signed_in? ? current_admin_user : 'Unknown user'
   end
  
-	
-  protect_from_forgery
+	protect_from_forgery
 end
