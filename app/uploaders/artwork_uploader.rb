f1140a0470a13d12a81bc6d9c6a770b139f9f38e
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-11-10 11:50:07 -0500

    fixed artwork size

--- app/uploaders/artwork_uploader.rb
+++ app/uploaders/artwork_uploader.rb
@@ -19,7 +19,7 @@ class ArtworkUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :artwork do
-    process :resize_to_limit => [300, 300]
+    process :resize_to_limit => [100, 100]
   end
 
 
