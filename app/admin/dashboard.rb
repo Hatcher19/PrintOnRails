e266a4c586569dad44a83361a0851b794fad70ef
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-11-26 17:31:25 -0500

    fixed dashboard table to only show orders within their account

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,14 +1,15 @@
 ActiveAdmin::Dashboards.build do
 	section "New Orders", :priority => 1 do
-		table_for Order.limit(20) do
+		table_for Order.where(:account_id => current_admin_user.account_id, :order_status_id => 1) do
 			column "Id" do |order| link_to order.id, admin_order_path(order) end
 			column "Name" do |order| link_to order.name, admin_order_path(order) end
 			column "Created At" do |order| order.created_at end
 			column "User" do |order| order.admin_user.email end
+			column "Status" do |order| order.order_status.name end
 		end
 	end
 	section "Recently updated" do
-		table_for Order.limit(20) do
+		table_for Order.where(:account_id => current_admin_user.account_id, 'updated_at > ?' created_at) do
 			column "Id" do |order| order.id end
 			column "name" do |order| order.name end
 			column "Name" do |order| order.updated_at end
