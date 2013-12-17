a066d5d5f02053120e8edfea78c656a97d9154f5
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-12-16 21:08:02 -0500

    added action mailer to prod. env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ PrintOnRails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Action Mailer
+  config.action_mailer.default_url_options = { :host => "http://printonrails.herokuapp.com/" }
 end
