7980fe1e0877686841be56a1ab4b247f0f1d4649
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-12-16 21:45:03 -0500

    see previous

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ PrintOnRails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Action Mailer
-  config.action_mailer.default_url_options = { :host => "http://printonrails.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "printonrails.com" }
 end
