60be6ce6be8e755dc1147b2c6194e38cecd8ad45
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2014-02-12 19:19:15 -0500

    added total accounts to accounts index

--- app/admin/accounts.rb
+++ app/admin/accounts.rb
@@ -3,11 +3,14 @@ ActiveAdmin.register Account do
   	menu :label => "Accounts", :if => proc{ can?(:destroy, Account) }, :priority => 2
 
 	index do 
+		span do accounts.count end
+		span do "-Total Accounts" end
 		column "ID", :sortable => :id do |account| link_to account.id, admin_account_path(account) end
 		column "Company", :sortable => :company do |account| link_to account.company, admin_account_path(account) end
 		column :orders do |account| account.orders.count end
 		column :customers do |account| account.customers.count end
 		column :Users do |account| account.admin_users.count end
+		column :created_at do |account| account.created_at.strftime("%b %d, %Y") end
 		# column(:email, sortable: :email) {|account| account.admin_user.where( role == "account_admin").email(account)}
 	end
 
